db9267ff4f7909027fea9e0310be32ac915daa21
Omar Boninsegna <omaraccgaming@gmail.com>
2025-08-15 19:50:07 +0200

    Draft homepage style

---
+++ src/app/card-group/card-group.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-group',
+  imports: [],
+  templateUrl: './card-group.component.html',
+  styleUrl: './card-group.component.scss',
+  standalone: true,
+})
+export class CardGroupComponent {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+import { CardGroupComponent } from '../card-group/card-group.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, CardComponent, CardGroupComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
